027696fa7a1ec1f85ec6535a4046c1d08fe3acc2
blugee <blugee>
2023-03-21 06:53:37 -0400

    change for mobile view

--- component/Layout/components/Header/Header.jsx
+++ component/Layout/components/Header/Header.jsx
@@ -15,8 +15,8 @@ const Header = () => {
                     <div className="row align-items-center d-flex">
                         <div className="col-lg-3">
                             <div className="header-logo">
-                                <Link className="main-logo" href='/'><img src="assets/images/HOC-DAO-LOGO.png" alt="" /> </Link>
-                                <Link className="stiky-logo" href='/'> <img src="assets/images/HOC-DAO-LOGO.png" alt="" /></Link>
+                                <Link className="main-logo" href='/'><img src="assets/images/HODL-DAO-LOGO.png" alt="" /> </Link>
+                                <Link className="stiky-logo" href='/'> <img src="assets/images/HODL-DAO-LOGO.png" alt="" /></Link>
                             </div>
                         </div>
                         <div className="col-lg-9">
@@ -36,7 +36,7 @@ const Header = () => {
                                                     <li><Link href='/roadmap'>road</Link></li>
                                                     <li><Link href='/contact'>contact</Link></li>
                                                     <li><Link href='/choose'>choose</Link></li>
-                                                    <li><Link href='faq'>faq</Link></li>
+                                                    <li><Link href='/faq'>faq</Link></li>
                                                     <li><Link href='/blogdetails'>blog details</Link></li>
                                                     <li><Link href='/bloggrid'>blog grid</Link></li>
                                                 </ul>
@@ -68,44 +68,30 @@ const Header = () => {
                 <div className="mobile-menu">
                     <nav className="itsoft_menu">
                         <ul className="nav_scroll">
-                            <li><a href="Javascript:void(0);">demo</a>
+                            <li><Link href='/'>Home</Link>
+                            </li>
+                            <li><Link href='/about'>about</Link>
+                            </li>
+                            <li><Link href='Javascript:void(0);'>pages</Link>
                                 <div className="sub-menu">
                                     <ul>
-                                        <li><a href="index.html">homepage one</a></li>
-                                        <li><a href="index-2.html">homepage two </a></li>
-                                        <li><a href="index-3.html">homepage three </a></li>
-                                        <li><a href="index-4.html">homepage four</a></li>
-                                        <li><a href="index-9.html">homepage five New</a></li>
-                                        <li><a href="index-6.html">animation page</a></li>
-                                        <li><a href="index-5.html">Landing Page 01</a></li>
-                                        <li><a href="index-7.html">Landing Page 02</a></li>
-                                        <li><a href="index-8.html">Home Video</a></li>
-                                        <li><a href="index-10.html">Home Particle</a></li>
+                                        <li><Link href='/about'>about us </Link></li>
+                                        <li><Link href='/team'>our team</Link></li>
+                                        <li><Link href='/token'>token</Link></li>
+                                        <li><Link href='/roadmap'>road</Link></li>
+                                        <li><Link href='/contact'>contact</Link></li>
+                                        <li><Link href='/choose'>choose</Link></li>
+                                        <li><Link href='/faq'>faq</Link></li>
+                                        <li><Link href='/blogdetails'>blog details</Link></li>
+                                        <li><Link href='/bloggrid'>blog grid</Link></li>
                                     </ul>
                                 </div>
                             </li>
-                            <li><a href="about.html">about</a>
+                            <li><Link href='/roadmap'>road map</Link>
                             </li>
-                            <li><a href="Javascript:void(0);">pages</a>
-                                <div className="sub-menu">
-                                    <ul>
-                                        <li><a href="about.html">about us </a></li>
-                                        <li><a href="team.html">our team</a></li>
-                                        <li><a href="token.html">token</a></li>
-                                        <li><a href="road-map.html">road</a></li>
-                                        <li><a href="contact.html">contact</a></li>
-                                        <li><a href="choose.html">choose</a></li>
-                                        <li><a href="faq.html">faq</a></li>
-                                        <li><a href="blog-details.html">blog details</a></li>
-                                        <li><a href="blog-grid.html">blog grid</a></li>
-                                    </ul>
-                                </div>
+                            <li><Link href='/team'>team</Link>
                             </li>
-                            <li><a href="road-map.html">road map</a>
-                            </li>
-                            <li><a href="team.html">team</a>
-                            </li>
-                            <li><a href="contact.html">contact</a></li>
+                            <li><Link href='/contact'>contact</Link></li>
                         </ul>
                     </nav>
                 </div>
@@ -116,4 +102,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
